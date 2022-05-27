8cf5e9b371ef8e41dfcee3e23958a3d493fdac0a
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-26 21:24:47 -0400

    updates to chat box

--- Main.js
+++ Main.js
@@ -1,8 +1,49 @@
 // Pseudo Code
 /*
 1. Create API --> Chat bot API
-2. Starting point is general high level question->predetermend answers
+2. Starting point is general high level question->predetermined answers
 3. Takes the user to the next branch.
 
-
+Q: How can I help you today?
+A1. Make a deposit.
+A2. Statement balance
+A3. Make a withdrawal 
 */
+
+document.querySelector("#clickMe").addEventListener("click", makeReq);
+
+async function makeReq() {
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?person=${userName}`);
+  const data = await res.json();
+
+  console.log(data);
+  document.querySelector("#a1").innerText = data.answer1;
+  document.querySelector("#a2").innerText = data.answer2;
+  document.querySelector("#a3").innerText = data.answer3;
+  //document.querySelector("#flipSide").textContent = data.flip;
+}
+
+fetch("/questions.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestion) => {
+    console.log(loadedQuestion);
+    question = loadedQuestion;
+    startGame();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
--- server.js
+++ server.js
@@ -1,3 +1,82 @@
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+// const querystring = require("querystring");
+// const figlet = require("figlet");
+
+// const server = http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   console.log(page);
+//   if (page == "/") {
+//     fs.readFile("index.html", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (page == "/otherpage") {
+//     fs.readFile("otherpage.html", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (page == "/otherotherpage") {
+//     fs.readFile("otherotherpage.html", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (page == "/api") {
+//     if ("student" in params) {
+//       if (params["student"] == "leon") {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         const objToJson = {
+//           name: "leon",
+//           status: "Boss Man",
+//           currentOccupation: "Baller",
+//         };
+//         res.end(JSON.stringify(objToJson));
+//       } //student = leon
+//       else if (params["student"] != "leon") {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         const objToJson = {
+//           name: "unknown",
+//           status: "unknown",
+//           currentOccupation: "unknown",
+//         };
+//         res.end(JSON.stringify(objToJson));
+//       } //student != leon
+//     } //student if
+//   } //else if
+//   else if (page == "/css/style.css") {
+//     fs.readFile("css/style.css", function (err, data) {
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (page == "/js/main.js") {
+//     fs.readFile("js/main.js", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/javascript" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else {
+//     figlet("404!!", function (err, data) {
+//       if (err) {
+//         console.log("Something went wrong...");
+//         console.dir(err);
+//         return;
+//       }
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+// });
+
+// server.listen(8000);
+
+
+
+
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
@@ -8,68 +87,62 @@ const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == "/") {
-    fs.readFile("index.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
+
+  let readWriteHead = (fileName, contentType) => {
+    fs.readFile(fileName, function (err, data) {
+      res.writeHead(200, { "Content-Type": contentType });
       res.write(data);
       res.end();
     });
-  } else if (page == "/otherpage") {
-    fs.readFile("otherpage.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (page == "/otherotherpage") {
-    fs.readFile("otherotherpage.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (page == "/api") {
-    if ("student" in params) {
-      if (params["student"] == "leon") {
+  };
+  switch (page) {
+    case "/":
+      readWriteHead("index.html", "text/html");
+      break;
+    case "/css/style.css":
+      fs.readFile("css/style.css", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/js/main.js":
+      readWriteHead("js/main.js", "text/javascript");
+      break;
+    case "/api":
+      let personName = "unknown";
+      let personStatus = "unknown";
+      let personOccupation = "unknown";
+
+      let objToJson = {
+        name: personName,
+        status: personStatus,
+        currentOccupation: personOccupation,
+      };
+
+      if (params["person"] == "leon") {
         res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
+        let objToJson = {
           name: "leon",
           status: "Boss Man",
           currentOccupation: "Baller",
+          flip: flipRes,
         };
         res.end(JSON.stringify(objToJson));
-      } //student = leon
-      else if (params["student"] != "leon") {
+      } else {
         res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown",
-        };
         res.end(JSON.stringify(objToJson));
-      } //student != leon
-    } //student if
-  } //else if
-  else if (page == "/css/style.css") {
-    fs.readFile("css/style.css", function (err, data) {
-      res.write(data);
-      res.end();
-    });
-  } else if (page == "/js/main.js") {
-    fs.readFile("js/main.js", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-      res.write(data);
-      res.end();
-    });
-  } else {
-    figlet("404!!", function (err, data) {
-      if (err) {
-        console.log("Something went wrong...");
-        console.dir(err);
-        return;
       }
-      res.write(data);
-      res.end();
-    });
+      break;
+    default:
+      figlet("404!!", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
+          console.dir(err);
+          return;
+        }
+        res.write(data);
+        res.end();
+      });
   }
 });
-
 server.listen(8000);
