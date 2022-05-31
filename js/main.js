5781ac9296ac1ad08eac857a894536daa50234ef
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-31 14:09:06 -0400

    updating Chat-Bot files

---
+++ js/main.js
@@ -0,0 +1,162 @@
+// Pseudo Code
+/*
+1. Create API --> Chat bot API
+2. Starting point is general high level question->predetermined answers
+3. Takes the user to the next branch.
+
+Q: How can I help you today?
+A1. Make a deposit.
+A2. Statement balance
+A3. Make a withdrawal 
+*/
+
+//other code
+
+
+// const q1 = document.querySelector("#q1");
+// const a1 = document.querySelector("#a1");
+// const a2 = document.querySelector("#a2");
+// const a3 = document.querySelector("#a3");
+
+// console.log(q1, a1, a2, a3);
+
+// function check(a) {
+//   console.log(` ${a}this was clicked`);
+// }
+
+// q1.addEventListener("click", check("a"));
+// a1.addEventListener("click", check("b"));
+// a2.addEventListener("click", check("c"));
+// a3.addEventListener("click", check("d"));
+
+
+
+//old code below
+//******************
+
+// let ansArr = document.querySelectorAll(".answer");
+
+// let newArr = Array.prototype.ansArr;
+
+// console.log(newArr);
+
+// document.querySelector("#clickMe").addEventListener("click", makeReq);
+
+// async function makeReq() {
+//   //   const userName = document.querySelector("#userName").value;
+//   const res = await fetch(`/api?person=${userName}`);
+//   const data = await res.json();
+
+//   console.log(data);
+//   document.querySelector("#a1").innerText = data.answer1;
+//   document.querySelector("#a2").innerText = data.answer2;
+//   document.querySelector("#a3").innerText = data.answer3;
+//   //document.querySelector("#flipSide").textContent = data.flip;
+// }
+
+// fetch("/questions.json")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((loadedQuestion) => {
+//     console.log(loadedQuestion);
+//     question = loadedQuestion;
+//     startGame();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// //CONSTANTS
+// const CORRECT_BONUS = 10;
+// const MAX_QUESTIONS = 3;
+
+// startGame = () => {
+//   questionCounter = 0;
+//   score = 0;
+//   availableQuesions = [...questions];
+//   getNewQuestion();
+// };
+// getNewQuestion();
+
+
+//Updates:
+document.querySelector('.reset').addEventListener('click', reset);
+
+let currentQuestionIndex = 0;
+const questionOutput = document.querySelector('.questionArea');
+const buttonsOutput = document.querySelector('.answerArea');
+
+async function renderQuestion() {
+  const res = await fetch(`/api?index=${currentQuestionIndex}`);
+  const question = await res.json();
+  questionOutput.textContent = question.text;
+  buttonsOutput.textContent = '';
+
+  for (let i = 0; i < question.answers.length; i++) {
+    const answer = question.answers[i];
+    const button = document.createElement('button');
+    button.textContent = answer.text;
+
+    buttonsOutput.appendChild(button);
+    button.addEventListener('click', function () {
+      switch (answer.next) {
+        case 'billing':
+          currentQuestionIndex = 1;
+          break;
+        case 'transfer':
+          currentQuestionIndex = 2;
+          break;
+        case 'info':
+          currentQuestionIndex = 3;
+          break;
+
+        case 'funds':
+          currentQuestionIndex = 4;
+          break;
+        case 'lateFee':
+          currentQuestionIndex = 5;
+          break;
+        case 'stop':
+          currentQuestionIndex = 6;
+          break;
+
+        case 'cToS':
+          currentQuestionIndex = 7;
+          break;
+        case 'sToC':
+          currentQuestionIndex = 8;
+          break;
+        case 'zelle':
+          currentQuestionIndex = 9;
+          break;
+
+        case 'stolen':
+          currentQuestionIndex = 10;
+          break;
+        case 'replacement':
+          currentQuestionIndex = 11;
+          break;
+        case 'hours':
+          currentQuestionIndex = 12;
+          break;
+
+        case 'transferPage':
+          window.location =
+            'https://citrine-bankchatbot.herokuapp.com/transfer';
+          break;
+        case 'replacementPage':
+          window.location =
+            'https://citrine-bankchatbot.herokuapp.com/replacement';
+          break;
+      }
+      renderQuestion();
+    });
+  }
+}
+
+renderQuestion();
+
+function reset() {
+  location.reload();
+}
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Take one
+
 // const http = require("http");
 // const fs = require("fs");
 // const url = require("url");
@@ -75,78 +77,156 @@
 // server.listen(8000);
 
 
+//Take two
 
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const querystring = require("querystring");
-const figlet = require("figlet");
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+// const querystring = require("querystring");
+// const figlet = require("figlet");
+
+// const server = http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   console.log(page);
+
+//   let readWriteHead = (fileName, contentType) => {
+//     fs.readFile(fileName, function (err, data) {
+//       res.writeHead(200, { "Content-Type": contentType });
+//       res.write(data);
+//       res.end();
+//     });
+//   };
+//   switch (page) {
+//     case "/":
+//       readWriteHead("index.html", "text/html");
+//       break;
+//     case "/css/style.css":
+//       fs.readFile("css/style.css", function (err, data) {
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     case "/js/main.js":
+//       readWriteHead("js/main.js", "text/javascript");
+//       break;
+//     case "/api":
+//       let a1 = [
+//         "Balance Amount",
+//         "Last Balance",
+//         "Last Deposit",
+//         "Last Withdrawal",
+//       ];
+//       let personStatus = "unknown";
+//       let personOccupation = "unknown";
+
+//       let objToJson = {
+//         name: personName,
+//         status: personStatus,
+//         currentOccupation: personOccupation,
+//       };
+
+//       if (params["person"] == "leon") {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         let objToJson = {
+//           name: "leon",
+//           status: "Boss Man",
+//           currentOccupation: "Baller",
+//           flip: flipRes,
+//         };
+//         res.end(JSON.stringify(objToJson));
+//       } else {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify(objToJson));
+//       }
+//       break;
+//     default:
+//       figlet("404!!", function (err, data) {
+//         if (err) {
+//           console.log("Something went wrong...");
+//           console.dir(err);
+//           return;
+//         }
+//         res.write(data);
+//         res.end();
+//       });
+//   }
+// });
+// server.listen(8000);
+
+
+
+//Updates:
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet');
 
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
 
-  let readWriteHead = (fileName, contentType) => {
-    fs.readFile(fileName, function (err, data) {
-      res.writeHead(200, { "Content-Type": contentType });
+  // read file and write to browser
+  const readHelper = (filename, contentType) => {
+    fs.readFile(filename, function (err, data) {
+      res.writeHead(200, { 'Content-Type': contentType });
       res.write(data);
       res.end();
     });
   };
-  switch (page) {
-    case "/":
-      readWriteHead("index.html", "text/html");
-      break;
-    case "/css/style.css":
-      fs.readFile("css/style.css", function (err, data) {
-        res.write(data);
-        res.end();
-      });
-      break;
-    case "/js/main.js":
-      readWriteHead("js/main.js", "text/javascript");
-      break;
-    case "/api":
-      let a1 = [
-        "Balance Amount",
-        "Last Balance",
-        "Last Deposit",
-        "Last Withdrawal",
-      ];
-      let personStatus = "unknown";
-      let personOccupation = "unknown";
 
-      let objToJson = {
-        name: personName,
-        status: personStatus,
-        currentOccupation: personOccupation,
-      };
+  //Landing page request
+  if (page == '/') {
+    readHelper('index.html', 'text/html');
+  } else if (page == '/transfer') {
+    readHelper('transfer.html', 'text/html');
+  } else if (page == '/replacement') {
+    readHelper('replacement.html', 'text/html');
+  }
 
-      if (params["person"] == "leon") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        let objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller",
-          flip: flipRes,
-        };
-        res.end(JSON.stringify(objToJson));
-      } else {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(objToJson));
-      }
-      break;
-    default:
-      figlet("404!!", function (err, data) {
+  // API Request
+  else if (page == '/api') {
+    if ('index' in params) {
+      fs.readFile('js/questions.json', 'utf8', (err, data) => {
         if (err) {
-          console.log("Something went wrong...");
-          console.dir(err);
-          return;
+          console.log(`Error reading file from disk: ${err}`);
+        } else {
+          const questions = JSON.parse(data);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(questions[params.index]));
         }
-        res.write(data);
-        res.end();
       });
+    }
+  }
+
+  // return the style.css file when requested
+  else if (page == '/css/style.css') {
+    fs.readFile('css/style.css', function (err, data) {
+      res.write(data);
+      res.end();
+    });
+  }
+
+  // return the main.js file when requested
+  else if (page == '/js/main.js') {
+    readHelper('js/main.js', 'text/javascript');
+  }
+
+  //Return 404 error if page is not found
+  else {
+    figlet('404!!', function (err, data) {
+      if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
   }
 });
-server.listen(8000);
+
+server.listen(process.env.PORT || 8000);
